61759e9a430fa7663e967c9a254a5f3be23864b3
Liquid <13621351+liqtags@users.noreply.github.com>
2024-10-12 22:49:17 +0100

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+export const ETH_UNISWAP_ROUTER_ADDRESS =
+  "0x7a250d5630b4cf539739df2c5dacb4c659f2488d";
+export const ETH_BANANA_ROUTER_ADDRESS =
+  "0x3328f7f4a1d1c57c35df56bbf0c9dcafca309c49";
+export const ETH_MAESTRO_ROUTER_ADDRESS =
+  "0x80a64c6d7f12c47b7c66c5b4e20e72bc1fcd5d9e";
