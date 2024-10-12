ff8011142cec1040753c4802e611d447d2af0e76
liquid <13621351+liqtags@users.noreply.github.com>
2024-10-12 23:11:51 +0100

    Deploy 0.0.2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
+/**
+ * @module dexlens/addressbook
+ * @author Dexlens <support@dexlens.io>
+ * @license MIT
+ * @description A simple address book for DexLens.
+ * @example
+ * import { ETH_UNISWAP_ROUTER_ADDRESS } from "jsr:@dexlens/addressbook";
+ */
+
+/**
+ * Ethereum Uniswap Router Address
+ */
 export const ETH_UNISWAP_ROUTER_ADDRESS =
   "0x7a250d5630b4cf539739df2c5dacb4c659f2488d";
+
+/**
+ * Ethereum Banana Router Address
+ */
 export const ETH_BANANA_ROUTER_ADDRESS =
   "0x3328f7f4a1d1c57c35df56bbf0c9dcafca309c49";
+
+/**
+ * Ethereum Maestro Router Address
+ */
 export const ETH_MAESTRO_ROUTER_ADDRESS =
   "0x80a64c6d7f12c47b7c66c5b4e20e72bc1fcd5d9e";
